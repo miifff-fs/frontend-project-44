4756d5a04bf4a7d84bc2dbfb08b4b49482ab6175
miifff <you@example.com>
2025-12-04 16:26:29 +0300

    Step 6

--- README.md
+++ README.md
@@ -6,5 +6,8 @@
 [![Reliability Rating](https://sonarcloud.io/api/project_badges/measure?project=miifff-fs_frontend-project-44&metric=reliability_rating)](https://sonarcloud.io/summary/new_code?id=miifff-fs_frontend-project-44)
 [![Maintainability Rating](https://sonarcloud.io/api/project_badges/measure?project=miifff-fs_frontend-project-44&metric=sqale_rating)](https://sonarcloud.io/summary/new_code?id=miifff-fs_frontend-project-44)
 
-### Пример работы:
+### Пример игры проверка на чётность:
 https://asciinema.org/a/ZPX1LftwNjtos5PFUhG6hW2rN
+
+### Пример игры калькулятор:
+https://asciinema.org/a/8oGShPJ0aRD1zwXfpGUyfRdKN
---
+++ src/games/calc.js
@@ -0,0 +1,23 @@
+export const ruleDescription = 'What is the result of the expression?'
+
+export const generateRound = () => {
+  const operators = ['+', '-', '*']
+  const operator = operators[Math.floor(Math.random() * 3)]
+  let number1 = Math.floor(Math.random() * 100) + 1
+  let number2 = Math.floor(Math.random() * 100) + 1
+
+  const question = `${number1} ${operator} ${number2}`
+  let result
+  if (operator === '+') {
+    result = number1 + number2
+  }
+  else if (operator === '-') {
+    result = number1 - number2
+  }
+  else {
+    result = number1 * number2
+  }
+  return {
+    question,
+    answer: String(result) }
+}
